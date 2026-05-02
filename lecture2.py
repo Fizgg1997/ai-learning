2787c58f40a4776eb52820784f7448cfdc38771c
Fizgg1997 <faizantanveer097@gmail.com>
2026-05-02 14:33:02 +0300

    This is Commit 3 of my Learning

---
+++ lecture2.py
@@ -0,0 +1,3 @@
+file = open("notes.text", "w")
+file.write("I Started learning AI code as and AI engineer i will make my journey more successful")
+file.close
---
+++ mypractice.py
@@ -0,0 +1,4 @@
+file = open("practicefile.text", "w")
+file.write("This is practice file to check how to make the code of the file and expect the ")
+
+file.close
