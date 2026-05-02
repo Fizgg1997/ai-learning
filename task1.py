8e04f550ede31497033d93f88cadcd4a8bbdd45a
Fizgg1997 <faizantanveer097@gmail.com>
2026-05-02 14:27:55 +0300

    This is my second File

---
+++ task1.py
@@ -0,0 +1,25 @@
+# Task 1
+
+# Store 5 student marks in list.
+marks = [50,60,80,100]
+# Task 2
+
+# Use loop to print each mark.
+for i in marks:
+    print(i)
+
+# Task 3
+
+# Print total marks.
+print(sum(marks))
+# Task 4
+
+# Print average marks.
+def average(mymarks):
+    total = sum(mymarks)
+    return total / len(mymarks)
+
+print(average(marks))
+# Task 5
+
+# Use if condition:
